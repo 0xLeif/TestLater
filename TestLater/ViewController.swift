166ab0158b30d4bcb8aeabe2da20dc74a3c0852b
zmeriksen <zmeriksen@icloud.com>
2020-08-04 17:45:59 -0500

    Later POC

--- TestLater/ViewController.swift
+++ TestLater/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import SwiftUIKit
+import Later
+import NIO
+import Combine
 
 class ViewController: UIViewController {
-
+    private let imageURL = "https://github.com/0xLeif/SwiftUIKit/blob/master/assets/SwiftUIKit_logo_v1.png?raw=true"
+    
+    private var bag = [AnyCancellable]()
+    
+    private lazy var futureImage: EventLoopFuture<UIImage?> = Later.default.promise { (promise) in
+        sleep(3)
+        URL(string: self.imageURL).map { url in
+            URLSession.shared.dataTaskPublisher(for: url)
+                .sink(receiveCompletion: { (event) in
+                    
+                    print(event)
+                    
+                }) { (data: Data, response: URLResponse) in
+                    
+                    guard let image = UIImage(data: data) else {
+                        promise.succeed(nil)
+                        return
+                    }
+                    
+                    promise.succeed(image)
+            }
+            .store(in: &self.bag)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.embed {
+            Label("Hello World!")
+                .text(alignment: .center)
+                .configure { label in
+                    
+                    Later.default.do(withDelay: 2) {
+                        DispatchQueue.main.async {
+                            label.text = "Later!"
+                        }
+                    }
+                    
+                    self.futureImage.always { result in
+                        switch result {
+                        case .success(let image):
+                            DispatchQueue.main.async {
+                                label.clear().embed {
+                                    image.map { Image($0) } ?? Image(.systemRed)
+                                }
+                            }
+                        default:
+                            print("-1")
+                        }
+                    }
+            }
+        }
     }
-
-
 }
-
